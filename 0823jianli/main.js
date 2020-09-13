c9f465542cf48da7ce359ff8fb2712a87315c1b1
yexy <584604539@qq.com>
2020-09-13 19:03:25 +0800

    day6

---
+++ 0823jianli/main.js
@@ -0,0 +1,22 @@
+
+portfolio1.onclick= function(){
+    portfolioBar.className = 'state-1'
+    picturejobs2.style.display = 'none'
+    picturejobs3.style.display = 'none'
+    picturejobs1.style.display = 'block'
+    zuopinji.style.height = '728px'
+}
+portfolio2.onclick= function(){
+    portfolioBar.className = 'state-2'
+    picturejobs1.style.display = 'none'
+    picturejobs3.style.display = 'none'
+    picturejobs2.style.display = 'block'
+    zuopinji.style.height = '728px'
+}
+portfolio3.onclick= function(){
+    portfolioBar.className = 'state-3'
+    picturejobs1.style.display = 'none'
+    picturejobs2.style.display = 'none'
+    picturejobs3.style.display = 'block'
+    zuopinji.style.height = '500px'
+} 
